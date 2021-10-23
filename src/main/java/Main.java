10ff37d9824da4d97b954489a554fd025fd9b12a
KaWa <Karollo47@gmail.com>
2021-10-23 22:55:05 +0200

    fixes, exception handler for picking category

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,41 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        // boolean variable for running this whole app in loop
         boolean isStillRunning = true;
 
-        // checking if app running or if it's stopped by user
+        // loop determines if app running
         while (isStillRunning) {
             Scanner scanner = new Scanner(System.in);
 
             System.out.println("Welcome in quiz by KaWa! :)");
 
-            /// TODO: 21.10.2021 more categories
-            System.out.println("Categories:");
-            System.out.println("1 - Film");
-            System.out.println("2 - Sport");
-            System.out.println("Chose category:");
-
-            //// boolean to check for illegalArgument exception loop while user choosing category
-            /// TODO: 22.10.2021 TryCatch for scanning number of cattegory form user
             boolean stillCheckingForUsersCategoryPick = true;
+            int chosenQuizCategory = 0;
 
-            // taking number of chosen category form user
-            int chosenQuizCategory = scanner.nextInt();
+            // loop to check user's category pick, handle exception for InputMismatch
+            while (stillCheckingForUsersCategoryPick) {
+                /// TODO: 21.10.2021 more categories
+                System.out.println("Categories:");
+                System.out.println("1 - Film");
+                System.out.println("2 - Sport");
+                System.out.println("Chose category:");
+
+                try {
+                    chosenQuizCategory = scanner.nextInt();
+                } catch (InputMismatchException inputMismatchException) {
+                    System.out.println("Wrong answer.");
+                } finally {
+                    if (chosenQuizCategory>=1 && chosenQuizCategory<=2) {
+                        stillCheckingForUsersCategoryPick = false;
+                    } else {
+                        System.out.println("It needs to be number of a available category.");
+                        scanner.nextLine();
+                    }
+                }
+            }
 
             // switching chosen category and running proper quiz
             /// TODO: 21.10.2021 complete with functions to actually run quiz and interact with user
@@ -32,21 +44,17 @@ public class Main {
                 case 2 -> System.out.println("Stuff happening with quiz for Sport category");
             }
 
-            // boolean to check for illegalArgument exception loop while user choosing to exit or play agian
             boolean stillCheckingIfUserWantToLeave = true;
-            // String for user's exit choice
             String playAgainOption = null;
 
-            // loop for IllegalArgument while user trying to exit and type something else than String
+            // loop to check user's answer (y/n), if he wants to end or not, handle exception for IlleaglArgument
             while (stillCheckingIfUserWantToLeave) {
                 System.out.println("Wanna play again? y/n");
 
-                // taking user answer (y/n), if he wants to end or not, handle exception for IlleaglArgument
                 try {
                     playAgainOption = scanner.next();
                 } catch (IllegalArgumentException illegalArgumentException) {
                     System.out.println("Wrong answer.");
-                    System.out.println("Press 'y' if you want to play again or press 'n' if you want to exit.");
                 } finally {
                     assert playAgainOption != null;
                     if (playAgainOption.equals("n")) {
@@ -57,14 +65,11 @@ public class Main {
                         System.out.println("---------------------------------------------------------------------");
                         stillCheckingIfUserWantToLeave = false;
                     } else {
-                        System.out.println("Wrong answer.");
                         System.out.println("Press 'y' if you want to play again or press 'n' if you want to exit.");
                     }
                 }
             }
         }
-
-
     }
 }
 
